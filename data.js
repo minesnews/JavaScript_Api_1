c300221f6b0179bdee4acc9ca1e966146754b47d
Nikolay Chelpanov <nikch3008@mail.ru>
2025-03-15 18:36:21 +0700

    add hw

---
+++ data.js
@@ -0,0 +1,80 @@
+export const workouts = `
+[
+    {
+        "id": "1",
+        "name_of_workout": "Веб-вёрстка HTML/CSS",
+        "time_of_workout": "09:30 - 11:30",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "5"
+    },
+    {
+        "id": "2",
+        "name_of_workout": "Продвинутый HTML + CSS",
+        "time_of_workout": "11:00 - 13:00",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "3"
+    },
+    {
+        "id": "3",
+        "name_of_workout": "Основы Javascript",
+        "time_of_workout": "11:00 - 13:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "18"
+    },
+    {
+        "id": "4",
+        "name_of_workout": "JavaScript про ECMAScript",
+        "time_of_workout": "10:00 - 12:00",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "5",
+        "name_of_workout": "Основы Node.js",
+        "time_of_workout": "11:00 - 12:00",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "8"
+    },
+    {
+        "id": "6",
+        "name_of_workout": "JavaScript про API браузеров",
+        "time_of_workout": "10:00 - 12:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "19"
+    },
+    {
+        "id": "7",
+        "name_of_workout": "Продвинутый JavaScript",
+        "time_of_workout": "09:30 - 11:30",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "13"
+    },
+    {
+        "id": "8",
+        "name_of_workout": "Фреймворк React JS",
+        "time_of_workout": "09:00 - 11:00",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "7"
+    },
+    {
+        "id": "9",
+        "name_of_workout": "Основы PHP",
+        "time_of_workout": "11:30 - 12:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "14"
+    },
+    {
+        "id": "10",
+        "name_of_workout": "Вокруг PHP – экосистема веб-приложений",
+        "time_of_workout": "15:30 - 17:30",
+        "max_number_of_participants": "50",
+        "current_number_of_participants": "32"
+    },
+    {
+        "id": "11",
+        "name_of_workout": "Продвинутое программирование на PHP — Laravel",
+        "time_of_workout": "20:30 - 22:30",
+        "max_number_of_participants": "11",
+        "current_number_of_participants": "2"
+    }
+]`
